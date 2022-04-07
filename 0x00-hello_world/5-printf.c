a71e2a903610014f58b223d7590c5be31e255813
danida21 <danidanih21@gmail.com>
2022-04-07 07:58:18 +0300

    5th task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entery point
- * printf 
- * return 1 after program execcution
+ * My hello. printf task
+ * Return: return 1 after program execution
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art.\n");
 return (0);
 }
