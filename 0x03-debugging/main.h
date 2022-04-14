a181f322b203251a2e5c24d79ee7ee5fab3d96b6
danida21 <danidanih21@gmail.com>
2022-04-14 07:15:17 +0300

    2st task  main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,1 +1,24 @@
-Executable File  0 lines (0 sloc)  0 Bytes
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest = b;
+
+	if (a > b)
+	{
+		largest = a;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
