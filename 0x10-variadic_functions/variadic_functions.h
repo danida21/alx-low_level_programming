7fb9ac5654f5f41b1df7e270ba0702c1480d959d
danida21 <101599585+danida21@users.noreply.github.com>
2022-05-12 21:28:32 +0300

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -22,4 +22,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 /* prints anything */
 void print_all(const char * const format, ...);
 
-#endif  /* MAIN_HEADER */
+#endif /* MAIN_HEADER */
